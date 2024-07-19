355f5a2764a75e2fe6ab03bf0f2eae71cb7af692
M-Rohr <rohrmatthias1@gmail.com>
2024-07-19 10:04:54 +0200

    Update simulation_experiment.py

--- simulation_experiment.py
+++ simulation_experiment.py
@@ -122,11 +122,11 @@ def par_simul(i, S, Fecundity, omega, sigma_s, sigma_h, Ext_seed_rain,
 # IV. Run the simulation in parallel
 # ===============================================================================    
 
-#### /!\ If you are working on windows, for some (obscure) reason python may return:
+#### /!\ If you are working on windows, python may return:
     # ModuleNotFoundError: No module named  'model_function'.
     # It'sa bug from the parallelization process. If it's appen, run the script with parameters set for very small 
     # simulations (eg. n = 5, S = 10, max_tick = 3) in debug mode, with a break point somewhere in par_simul().
-    # Then the script should run correctly in 'normal' mode, for this session/console.
+    # Then the script should the script should run properly.
 
 ## Simulation experiment can take from several minutes to several hours to run depending on :
     # Number of simulations 
